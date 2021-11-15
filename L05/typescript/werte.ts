90f0c6d21b38958ca831cdac8bba10b209e9d188
elevou <elena.maria.voulgaridis@hs-furtwangen.de>
2021-11-15 22:17:15 +0100

    Anpassung

--- L05/typescript/werte.ts
+++ L05/typescript/werte.ts
@@ -58,34 +58,34 @@ var welt : number = af18+sa18+eu18+na18+as18+au18;
 
 /* Console */ 
 console.log ('Die Emission von Afrika ist:' +af18+ 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit'+AfWelt+'%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um'+Af08Af18pr+'% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das'+Af08Af18kg+'kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' +AfWelt+ '%');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' +Af08Af18pr+ '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das' +Af08Af18kg+ 'kg CO2');
 
 console.log ('Die Emission von Süd Amerika ist:' +sa18+ 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit'+SaWelt+'%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um'+Sa08Sa18pr+'% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das'+Sa08Sa18kg+'kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' +SaWelt+ '%');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' +Sa08Sa18pr+ '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das' +Sa08Sa18kg+ 'kg CO2');
 
 console.log ('Die Emission von Europa ist:' +eu18+ 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit'+EuWelt+'%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um'+Eu08Eu18pr+'% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das'+Eu08Eu18kg+'kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' +EuWelt+ '%');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' +Eu08Eu18pr+ '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das' +Eu08Eu18kg+ 'kg CO2');
 
 console.log ('Die Emission von Nord Amerika ist:' +na18+ 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit'+NaWelt+'%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um'+Na08Na18pr+'% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das'+Na08Na18kg+'kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' +NaWelt+ '%');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' +Na08Na18pr+ '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das' +Na08Na18kg+ 'kg CO2');
 
 console.log ('Die Emission von Asien ist:' +as18+ 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit'+AsWelt+'%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um'+As08As18pr+'% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das'+As08As18kg+'kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' +AsWelt+ '%');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' +As08As18pr+ '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das' +As08As18kg+ 'kg CO2');
 
 console.log ('Die Emission von Austarlien ist:' +au18+ 'kg CO2');
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit'+AuWelt+'%');
-console.log('Fuer Afrika hat sich 2018 im Vergleich zu 2008 die Emission um'+Au08Au18pr+'% verändert.');
-console.log('2018 im Vergleich zu 2008 sind das'+Au08Au18kg+'kg CO2');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit' +AuWelt+ '%');
+console.log('Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um' +Au08Au18pr+ '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das' +Au08Au18kg+ 'kg CO2');
 
 
 
